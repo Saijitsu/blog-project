78a219986156b33984d25c67c9b6f6a94e83762b
Nicolas Turlet <35563943+Saijitsu@users.noreply.github.com>
2018-11-08 15:32:03 +0100

    initial commit/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog-project';
+  title = 'Posts';
+
+  Posts = [
+    new Post('Mon premier post',
+            'Haec quaesita reperiuntur in sacris Israelis scriptionibus, at insunt etiam scriptis Veda necnon Avesta' +
+            'Secundum hodie vigentem interpretationem carmina hunc in Librum inclusa primitus fuerunt cantus ',
+            1,
+            null),
+    new Post('Mon deuxième post',
+      'Haec quaesita reperiuntur in sacris Israelis scriptionibus, at insunt etiam scriptis Veda necnon Avesta' +
+      'Secundum hodie vigentem interpretationem carmina hunc in Librum inclusa primitus fuerunt cantus ',
+      -1,
+      null),
+    new Post('Mon dernier post',
+      'Haec quaesita reperiuntur in sacris Israelis scriptionibus, at insunt etiam scriptis Veda necnon Avesta' +
+      'Secundum hodie vigentem interpretationem carmina hunc in Librum inclusa primitus fuerunt cantus .',
+      0,
+      null)
+  ];
 }
+export class Post {
+  constructor(
+    public title: string,
+    public content: string,
+    public loveIts: number,
+    public created_at: Date
+  ) {
+    this.created_at = new Date();
+  }
+}
+
